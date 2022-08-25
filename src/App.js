c1ebe9aa08ecb9ab56e7b991c3466312e12273e1
rounak sharma <rounak.0734@gmail.com>
2022-08-25 14:53:26 +0530

    Styled 'App.js' (External Styling)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import Header from './Header';
+import './App.css';
 
 function App() {
   return (
-    <>
+    <div>
       <Header />
-      <div>
-        <button>Add</button>
-        <span>Name</span>
-        <span>Phone</span>
+      <div className='container'>
+        <button className='add-btn'>Add</button>
+        <div className='grid-container head-section'>
+          <span className='grid-item name-heading'>Name</span>
+          <span className='grid-item phone-heading'>Phone</span>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
