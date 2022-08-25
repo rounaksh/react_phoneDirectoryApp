b027a75e42983a85af22f27287f8ef9ffefd35ee
rounak sharma <rounak.0734@gmail.com>
2022-08-25 15:20:30 +0530

    Rendered Contacts Dynamically

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
+import React, { Component } from 'react';
 import Header from './Header';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <div className='container'>
-        <button className='add-btn'>Add</button>
-        <div className='grid-container head-section'>
-          <span className='grid-item name-heading'>Name</span>
-          <span className='grid-item phone-heading'>Phone</span>
+class App extends Component {
+  render() {
+    let contacts = [
+      {
+        id: 1,
+        name: 'Rony',
+        contactNo: '91xxxxxxx0'
+      },
+      {
+        id: 2,
+        name: 'Jennifer',
+        contactNo: '91xxxxxxx0'
+      },
+      {
+        id: 2,
+        name: 'Jhon',
+        contactNo: '91xxxxxxx0'
+      }
+    ];
+    return (
+      <div>
+        <Header />
+        <div className='container'>
+          <button className='add-btn'>Add</button>
+          <div className='grid-container head-section'>
+            <span className='grid-item name-heading'>Name</span>
+            <span className='grid-item phone-heading'>Phone</span>
+          </div>
+          {
+            contacts.map(details => {
+              return <div className='grid-container' key={details.id}>
+                        <span className='grid-item'>{details.name}</span>
+                        <span className='grid-item'>{details.contactNo}</span>
+                      </div>
+            })
+          }
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
 export default App;
