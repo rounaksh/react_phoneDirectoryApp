37974b91787643b7534b700932a33b04d95469cb
rounak sharma <rounak.0734@gmail.com>
2022-08-27 16:35:53 +0530

    Styled 'AddContact' Component

--- src/AddContact.js
+++ src/AddContact.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import './AddContact.css'
 
 class AddContact extends Component {
     render() {
@@ -8,6 +9,22 @@ class AddContact extends Component {
                 <Header heading="Add Contact" />
                 <div className='container'>
                     <button className='btn'>back</button>
+                    <form className='add-contact-form'>
+                        <div className='form-group'>
+                            <label htmlFor='name' className='form-label'>Name: </label>
+                            <input id='name' className='form-input' name='name' type="text" />
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='contact' className='form-label'>Contact: </label>
+                            <input id='contact' className='form-input' name='contact' type="text" />
+                        </div>
+                        <div className='subscriber-info'>
+                            <span className='subscriber-info-heading'>Contact to be added </span>
+                            <span className='subscriber-name'>Name: </span>
+                            <span className='subscriber-contact'>Contact: </span>
+                        </div>
+                        <button type='submit' className='btn add-btn'>Add</button>
+                    </form>
                 </div>
             </div>
         )
